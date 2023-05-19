d1165283f127155818328bf56b7bcb3a8b753767
Angel <mra7588@gmail.com>
2023-05-18 17:08:36 -0700

    feat: add component loading

--- src/components/ListNews.tsx
+++ src/components/ListNews.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import {FlatList, Text, View, StyleSheet} from 'react-native';
+import React from 'react';
+import {FlatList, View, StyleSheet, ActivityIndicator} from 'react-native';
 import ItemNews from './ItemNews';
 import { useNews } from '../hooks/useNews';
 
@@ -7,8 +7,10 @@ import { useNews } from '../hooks/useNews';
 
 const styles = StyleSheet.create({
     container: {
-      flexDirection: 'column',
-      columnGap: 20
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+      
     },
 });
 
@@ -16,12 +18,11 @@ const ListNews = ({navigation}: any): JSX.Element => {
 
   const {articles, loading } = useNews()
   
-
   return (
-    <View >
+    <View style={styles.container}>
       {
         loading
-        ? <Text>Cargando...</Text>
+        ? <ActivityIndicator animating={loading} color="white" size="large"/>
         : <FlatList
           data={articles} 
           renderItem={(article)=><ItemNews navigation={navigation} article={article.item}/>}
